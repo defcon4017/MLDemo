f0a0dd149fb20812c8853345b7ac81f255d7b88b
defcon4017 <152660034+defcon4017@users.noreply.github.com>
2023-12-02 22:02:02 -0600

    Update ml_app.py

--- ml_app.py
+++ ml_app.py
@@ -1,7 +1,12 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from tensorflow import keras
-from tensorflow.keras import layers
+from tensorflow.python.keras.layers import Dense
+from tensorflow.python.keras import Sequential
+
+# Build model
+model = Sequential()
+model.add(Dense(1)) 
+model.compile(loss='mse')  
 
 st.write("ML Application")
